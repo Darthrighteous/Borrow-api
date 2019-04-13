35ecfbd53f63ef3219ab7c171ee98b16511eae1c
darthrighteous <ogunniyiabduljalil@yahoo.com>
2019-04-13 16:35:25 +0100

    update installment status and credit score

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -4,22 +4,45 @@
 class PaymentsController < ApplicationController
   include PaymentsControllerHelper
 
-  def create
-    installment = load_installment params[:installment_id]
+  before_action :load_installment
 
-    if installment.amount == params[:amount].to_i
-      payment = create_payment installment.id
+  def create
+    if @installment[:amount] == params[:amount].to_i
+      create_payment
+      update_installment_status
+      update_credit_score
+
       respond status: 'success', message: 'payment successfully made',
-              payment: payment.as_json(include: [:installment])
+              payment: @payment.as_json(include: [:installment])
     else
-      respond status: 'failure', installment: installment,
-              message: 'incorrect amount for that installment'
+      respond({ status: 'failure', installment: @installment,
+                message: 'incorrect amount for that installment' }, :forbidden)
     end
   end
 
   private
 
-  def create_payment(installment_id)
-    Payment.create(installment_id: installment_id)
+  def create_payment
+    @payment = Payment.create(
+      installment_id: @installment[:id],
+      created_at: params[:date_paid]
+    )
+  end
+
+  def update_installment_status
+    sts = @payment[:created_at] > @installment[:due_date] ? 'overdue' : 'paid'
+    @installment.update(status: sts)
+  end
+
+  def update_credit_score
+    if @installment[:status] == 'paid'
+      @installment.loan.user.increment('credit_score', 15)
+    else
+      @installment.loan.user.decrement('credit_score', 20)
+    end
+  end
+
+  def load_installment
+    @installment = Installment.find(params[:installment_id])
   end
 end
