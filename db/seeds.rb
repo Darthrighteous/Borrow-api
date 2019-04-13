c01af13e844f0161a30d4bd361316034811df636
darthrighteous <ogunniyiabduljalil@yahoo.com>
2019-04-13 07:38:12 +0100

    add user seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+users = [
+  {
+    name: 'Nnamdi Azikwe',
+    email: 'nnamdi.azikwe@borrow.com',
+    phone_no: '+2348094762345',
+    income: 50_000,
+    expenses: 25_000,
+    credit_score: 750
+  },
+  {
+    name: 'Bill gates',
+    email: 'bill.gates@borrow.com',
+    phone_no: '+2347064733999',
+    income: 1_000_000,
+    expenses: 200_000,
+    credit_score: 700
+  },
+  {
+    name: 'David Mardling',
+    email: 'david.mardling@borrow.com',
+    phone_no: '+2348063453232',
+    income: 500_000,
+    expenses: 400_000,
+    credit_score: 600
+  }
+]
+
+User.create(users)
