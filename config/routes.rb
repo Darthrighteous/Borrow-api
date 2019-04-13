51e3b6f1dedee3ca11829c6a01a72fa18663d106
darthrighteous <ogunniyiabduljalil@yahoo.com>
2019-04-13 12:03:50 +0100

    add route to get all users

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   scope '/api/v1' do
+    get '/users' => 'users#index'
     get '/user' => 'users#show'
 
     post '/loan' => 'loans#create'
